a169d47382412af180a63578e89e9d5548cff6c1
felix01.yu <felix01.yu@vipshop.com>
2025-12-30 14:42:52 +0800

    update

--- nano_gen.py
+++ nano_gen.py
@@ -1,9 +1,12 @@
+import sys
+import time
 from nanovllm import LLM, SamplingParams
 
 from prompt import text as aprompt
 
 
 def main(ckpt):
+    count = int(sys.argv[1])
     llm = LLM(ckpt, enforce_eager=True, tensor_parallel_size=1)
     sampling_params = SamplingParams(temperature=0.6, max_tokens=5, ignore_eos=True)
     n = 20
@@ -11,7 +14,13 @@ def main(ckpt):
     params_lst = [sampling_params for _ in range(n)]
     outputs = llm.generate(prompt_lst, params_lst)
     # outputs[0]["text"]
-    print(outputs[0]["text"])
+
+    begin = time.time()
+    for i in range(count):
+        outputs = llm.generate(prompt_lst, params_lst)
+        print(i, len(outputs), len(outputs[0]["token_ids"]))
+    end = time.time()
+    print(f"generate count {count}, mean time {(end - begin) / count:.3f}")
 
 
 if __name__ == '__main__':

dd97ab290de20fb6f3de653b3f5d52f83ec0d02c
felix01.yu <felix01.yu@vipshop.com>
2025-12-30 14:44:58 +0800

    update

--- nano_gen.py
+++ nano_gen.py
@@ -7,22 +7,25 @@ from prompt import text as aprompt
 
 def main(ckpt):
     count = int(sys.argv[1])
-    llm = LLM(ckpt, enforce_eager=True, tensor_parallel_size=1)
-    sampling_params = SamplingParams(temperature=0.6, max_tokens=5, ignore_eos=True)
+    llm = LLM(ckpt, enforce_eager=False, tensor_parallel_size=1)
+    sampling_params = SamplingParams(temperature=1.0, max_tokens=5, ignore_eos=True)
     n = 20
-    prompt_lst = [aprompt for _ in range(n)]
+    prompt_lst = [aprompt[::-1] for _ in range(n)]
     params_lst = [sampling_params for _ in range(n)]
     outputs = llm.generate(prompt_lst, params_lst)
-    # outputs[0]["text"]
 
     begin = time.time()
     for i in range(count):
+        prompt_lst = [aprompt[i:] for _ in range(n)]
+        start = time.time()
         outputs = llm.generate(prompt_lst, params_lst)
-        print(i, len(outputs), len(outputs[0]["token_ids"]))
+        finish = time.time()
+        print(i, len(outputs), len(outputs[0]["token_ids"]), f"{finish - start:.3f}")
     end = time.time()
     print(f"generate count {count}, mean time {(end - begin) / count:.3f}")
 
 
 if __name__ == '__main__':
     ckpt = "Qwen/Qwen3-1.7B"
+    # ckpt = '/root/autodl-tmp/Qwen3-1.7B'
     main(ckpt)
